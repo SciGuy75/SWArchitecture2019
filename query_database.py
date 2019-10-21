033714163639df23c04d8ad2c70221fe1d9377c6
Seryn13 <45214433+Seryn13@users.noreply.github.com>
2019-10-21 11:44:43 -0500

    Update query_database.py

--- query_database.py
+++ query_database.py
@@ -1,4 +1,10 @@
 def queryDatabase(query):
-    
+    # Connect to the database
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    # Save changes made and close the database
+    conn.commit()
+    conn.close()
 
     return
